2cfea94b67c324117d71dc0d5231f4086d089cd6
Mahdi Razi Gandomani <mahdi.razi099@gmail.com>
2025-12-23 14:33:44 +0330

    Update ewc.py

--- ewc.py
+++ ewc.py
@@ -10,7 +10,6 @@ class EWC():
         self.fisher = {}
         for name, param in model.named_parameters():
             self.prev_params[name] = param.detach().clone()
-    
     def penalty(self, model):
         if not self.fisher:
             return 0.0
@@ -20,9 +19,9 @@ class EWC():
             if name in self.fisher:
                 reg += (self.fisher[name] * (param - self.prev_params[name]) ** 2).sum()
         return 0.5 * self.lam * reg
-    
     def on_task_end(self, model, dataloader, crite, device):
         fisher = {}
+        counts = {}
         model.eval()
         total_seen = 0
         
@@ -32,38 +31,30 @@ class EWC():
             output = model(data)
             loss = crite(output, target)
             loss.backward()
-            
             for name, param in model.named_parameters():
                 if param.grad is None:
                     continue
-
                 grad = param.grad.detach() ** 2
                 if name not in fisher:
                     fisher[name] = grad.clone()
+                    counts[name] = 1
                 else:
-                    fisher[name]+= grad
-            
+                    fisher[name] += grad
+                    counts[name] += 1
             total_seen += data.size(0)
             if total_seen >= self.samples:
                 break
-        
-
         with torch.no_grad():
             for name in fisher:
-                fisher[name] /= total_seen
-            
+                fisher[name] /= max(counts[name], 1)
+            # Online update
             if self.fisher:
                 for name in fisher:
-                    self.fisher[name] = self.fisher[name] + fisher[name]
+                    self.fisher[name] = fisher[name] + self.fisher.get(name, 0.0)
             else:
                 self.fisher = fisher
             
-            
-            # Store current parameters
             for name, param in model.named_parameters():
                 self.prev_params[name] = param.detach().clone()
-
-
-
 def get_ewc(cfg, model, device):
         return EWC(model,device,lam=cfg.get('lam', 1000.0),samples=cfg.get('samples', 200))
